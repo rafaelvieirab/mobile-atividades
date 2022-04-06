17d37e221ebe79b4a3b0ba6bc8fe3989f56db028
Rafael Vieira Barbosa <rafaelviebar@gmail.com>
2022-04-05 22:53:15 -0300

    feat: resolvendo issue_1 modulo 1

--- Modulo_I/prova/issue_1/App.tsx
+++ Modulo_I/prova/issue_1/App.tsx
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = React.useState<number>(0);
+
+  const incrementCount = () => setCount(count+1);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.group}>
+        <Text style={styles.text}> You've pressed the button: {count} time(s)</Text>
+        <Button 
+          onPress={incrementCount} 
+          title="Press me" 
+          accessibilityLabel="Pressione para incrementar o valor do count" 
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +24,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    color: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  group: {
+    flex: 1,
+    marginTop: 64,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  text: {
+    marginTop: 32,
+    marginBottom: 24,
   },
 });
